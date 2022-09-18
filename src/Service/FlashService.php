487fe75e435c9fc8611021f8ea3b2bcbef62eae1
atournayre <aurelien.tournayre@gmail.com>
2022-09-18 11:44:20 +0200

    Ajout de FlashService pour mettre à disposition des methodes de création de flash message.

---
+++ src/Service/FlashService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Atournayre\Helper\Service;
+
+use Atournayre\Helper\Exception;
+use Symfony\Component\HttpFoundation\Exception\SessionNotFoundException;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class FlashService
+{
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @param Exception $exception
+     *
+     * @return void
+     * @throws \LogicException
+     */
+    public function addFromException(Exception $exception)
+    {
+        $this->add($exception->getType(), $exception->getMessage());
+    }
+
+    /**
+     * Adds a flash message to the current session for type.
+     *
+     * @param string $type
+     * @param string $message
+     *
+     * @return void
+     * @throws \LogicException
+     */
+    protected function add(string $type, string $message)
+    {
+        try {
+            $this->requestStack->getSession()->getFlashBag()->add($type, $message);
+        } catch (SessionNotFoundException $e) {
+            throw new \LogicException('You cannot use the addFlash method if sessions are disabled. Enable them in "config/packages/framework.yaml".', 0, $e);
+        }
+    }
+}

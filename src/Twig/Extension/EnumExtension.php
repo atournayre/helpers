8429468bb05f64b5eb602a4318fd2016e5c02693
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2022-10-23 15:40:42 +0200

    Ajout de la méthode getEnumCss() dans EnumExtension + suppression de la possibilité d'utiliser la méthode get*Readable() pour récupérer la valeur d'un Enum.

--- src/Twig/Extension/EnumExtension.php
+++ src/Twig/Extension/EnumExtension.php
@@ -10,10 +10,13 @@ use Twig\TwigFunction;
 
 class EnumExtension extends AbstractExtension
 {
+    private const METHOD_TO_GET_CSS_FOR_ENUM = 'css';
+
     public function getFunctions(): array
     {
         return [
             new TwigFunction('enum_value', [$this, 'getEnumValue']),
+            new TwigFunction('enum_css', [$this, 'getEnumCss']),
         ];
     }
 
@@ -29,24 +32,44 @@ class EnumExtension extends AbstractExtension
         $reflection = new ReflectionClass($object);
 
         $getter = 'get'.ucfirst($property);
-        $getterReadable = $getter.'Readable';
-        if ($reflection->hasMethod($getterReadable)) {
-            return $object->$getterReadable();
-        }
 
         $getterEnum = $getter.'Enum';
-        if ($reflection->hasMethod($getterEnum)) {
-            try {
-                $reflectionEnum = new ReflectionClass($object->$getterEnum());
-                if (!$reflectionEnum->isEnum()) return $object->$getter();
-
-                $enum = constant($reflectionEnum->getName() . '::' . $object->$getter());
-                return $enum->value;
-            } catch (ReflectionException $reflectionException) {
-                throw new Exception(sprintf('Unable to retrieve enum value, you must implement "%s()" into %s.', $getterEnum, get_class($object)), $reflectionException->getCode(), $reflectionException);
-            }
+        if (!$reflection->hasMethod($getterEnum)) {
+            throw new Exception(sprintf('Unable to retrieve enum value, you must implement "%s()" into %s.', $getterEnum, get_class($object)));
         }
 
-        throw new Exception(sprintf('Unable to retrieve enum value, you must implement "%s()" or "%s()" into %s.', $getterReadable, $getterEnum, get_class($object)));
+        try {
+            $reflectionEnum = new ReflectionClass($object->$getterEnum());
+            if (!$reflectionEnum->isEnum()) return $object->$getter();
+
+            $enum = constant($reflectionEnum->getName() . '::' . $object->$getter());
+            return $enum->value;
+        } catch (ReflectionException $reflectionException) {
+            throw new Exception(sprintf('Unable to retrieve enum value, you must implement "%s()" into %s.', $getterEnum, get_class($object)), $reflectionException->getCode(), $reflectionException);
+        }
+    }
+
+    /**
+     * @param object $object
+     * @param string $property
+     *
+     * @return string|null
+     * @throws Exception
+     */
+    public function getEnumCss(object $object, string $property): ?string
+    {
+        $reflection = new ReflectionClass($object);
+
+        $getterEnum = 'get'.ucfirst($property).'Enum';
+
+        if (!$reflection->hasMethod($getterEnum)) return '';
+
+        $enum = $object->$getterEnum();
+        $reflectionEnum = new ReflectionClass($enum);
+
+        if (!$reflectionEnum->hasMethod(self::METHOD_TO_GET_CSS_FOR_ENUM)) return '';
+
+        $method = self::METHOD_TO_GET_CSS_FOR_ENUM;
+        return $enum->$method();
     }
 }

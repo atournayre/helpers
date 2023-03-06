823c56eb4b30e4b2ee21b8e0d384cc49e50aa676
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2023-03-06 17:25:19 +0100

    Ajout de Assert::isCollectionOf() + refacto

--- src/Assert/Assert.php
+++ src/Assert/Assert.php
@@ -2,33 +2,48 @@
 
 namespace Atournayre\Helper\Assert;
 
+use Doctrine\Common\Collections\Collection;
+use function sprintf;
+
 class Assert extends \Webmozart\Assert\Assert
 {
     /**
-     * @param array  $collection
+     * @param array  $array
      * @param string $expectedClass
      * @param string $message
      *
      * @return void
      */
-    public static function isListOf(array $collection, string $expectedClass, string $message = ''): void
+    public static function isListOf(array $array, string $expectedClass, string $message = ''): void
     {
         $message = $message ?: sprintf('Expected list - non-associative array of %s.', $expectedClass);
-        static::isList($collection, $message);
-        static::allIsInstanceOf($collection, $expectedClass, $message);
+        static::isList($array, $message);
+        static::allIsInstanceOf($array, $expectedClass, $message);
     }
 
     /**
-     * @param array  $collection
+     * @param array  $array
      * @param string $expectedClass
      * @param string $message
      *
      * @return void
      */
-    public static function isMapOf(array $collection, string $expectedClass, string $message = ''): void
+    public static function isMapOf(array $array, string $expectedClass, string $message = ''): void
     {
         $message = $message ?: sprintf('Expected map - associative array with string keys of %s.', $expectedClass);
-        static::isMap($collection, $message);
-        static::allIsInstanceOf($collection, $expectedClass, $message);
+        static::isMap($array, $message);
+        static::allIsInstanceOf($array, $expectedClass, $message);
+    }
+
+    /**
+     * @param Collection $collection
+     * @param string     $expectedClass
+     * @param string     $message
+     *
+     * @return void
+     */
+    public static function isCollectionOf(Collection $collection, string $expectedClass, string $message = ''): void
+    {
+        static::allIsInstanceOf($collection->toArray(), $expectedClass, $message);
     }
 }
